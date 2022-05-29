c48ab2f6ef1bd8d5945fd0051f173b593c0844db
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-05-29 17:05:51 -0300

    init projeto

---
+++ main.py
@@ -0,0 +1,27 @@
+from modules.individual.models.individual import Individual
+from modules.population.models.population import Population
+from modules.world.models.world import World
+from modules.world.controller.world_controller import WorldController
+
+def main():
+
+    try:
+        # world = World(size=10, withParede=True)
+        worldController = WorldController()
+        # world.generateWorld()
+        # worldController.saveWorld(world= world)
+        worldFile = worldController.getWorldFile()
+        worldFile.printWorld()
+
+        # definir tamanho da polução    
+        population = Population(size=3)
+
+        population.generatePopulation()
+
+        population.getBestIndividual(worldFile)
+
+    except Exception as e:
+        print(e)
+    
+
+main()
